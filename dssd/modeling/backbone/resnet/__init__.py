a8e854cfa432016c298933730bb13faaf36e851f
t9s9 <schaumloeffel.tim@web.de>
2021-03-06 13:08:44 +0100

    first commit

--- dssd/modeling/backbone/resnet/__init__.py
+++ dssd/modeling/backbone/resnet/__init__.py
@@ -7,3 +7,8 @@ __all__ = ['resnet101']
 @registry.BACKBONES.register('resnet101')
 def resnet101(cfg, pretrained=True):
     return resnet.resnet101(pretrained=pretrained)
+
+
+@registry.BACKBONES.register('resnet50')
+def resnet50(cfg, pretrained=True):
+    return resnet.resnet50(pretrained=pretrained)
